92d9df77eda97ec544305382a50fe0b478ccccf2
jefferson <jeffer4533@gmail.com>
2023-07-25 17:13:35 -0500

    hice varios cambios de estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import Body from "./components/header/Body/Body";
 import Header from "./components/header/Header";
+
 
 function App() {
   return (
     <>
       <div>
         <Header />
+        <Body />
       </div>
     </>
   );
---
+++ src/components/header/Body/Body.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Body.css";
+
+const body: React.FC = () => {
+  return (
+    <div className="body-container">
+      <div className="content">
+        <p> Hombre </p>
+        <p> Mujer</p>
+      </div>
+    </div>
+  );
+};
+export default body;
